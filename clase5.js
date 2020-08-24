518e832e5b88521781823dbe9cbd4509e54b343a
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-08-24 22:05:26 +0200

    funciones

---
+++ clase5.js
@@ -0,0 +1,10 @@
+var nombre = 'Sacha';
+
+function imprimirNombreEnMayusculas(nombre) {
+  nombre = nombre.toUpperCase();
+  console.log(nombre);
+}
+
+imprimirNombreEnMayusculas(nombre);
+
+console.log(nombre);
---
+++ funciones.js
@@ -0,0 +1,10 @@
+var nombre = 'Jose';
+var edad = 22;
+
+function imprimirEdad(nombre, edad) {
+  console.log(`${nombre} tiene ${edad} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Luisa', 24);
+imprimirEdad(25, 'Luis');
